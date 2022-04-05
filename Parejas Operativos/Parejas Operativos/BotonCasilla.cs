3a491b1a7fc89f9dcbb12043efb6e1e73c766d57
Jonathan Guzmán <jonguz5@hotmail.com>
2022-04-04 20:38:06 -0600

    ejercicio completo

---
+++ Parejas Operativos/Parejas Operativos/BotonCasilla.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Parejas_Operativos
+{
+    // heredamos de Button
+    internal class BotonCasilla : Button
+    {
+        // constantes de colores
+        // - seleccionado -
+        private static readonly Color colorFondoSeleccionado = Color.FromHex("#7bcb03");
+        private static readonly Color colorTextoSeleccionado = Color.FromHex("#282828");
+        private static readonly FontAttributes atributosSeleccionado = FontAttributes.Bold;
+        // - no seleccionado -
+        private static readonly Color colorFondoNormal = Color.FromHex("#333333");
+        private static readonly Color colorTextoNormal = Color.FromHex("#ffffff");
+        private static readonly FontAttributes atributosNormal = FontAttributes.None;
+        // - bloqueado -
+        private static readonly Color colorFondoBloqueado = Color.FromHex("#51bbfe");
+        private static readonly Color colorTextoBloqueado = Color.FromHex("#282828");
+        private static readonly FontAttributes atributosBloqueado = FontAttributes.Bold | FontAttributes.Italic;
+
+        int fila;
+        int columna;
+        bool seleccionado = false;
+        bool bloqueado = false;
+        readonly string letra;
+
+        public BotonCasilla(int fila, int columna, string letra)
+        {
+            this.fila = fila;
+            this.columna = columna;
+            this.letra = letra;
+            Text = "-";
+        }
+
+        public int Fila
+        {
+            get { return fila; }
+            set { fila = value; }
+        }
+        public int Columna
+        {
+            get { return columna; }
+            set { columna = value; }
+        }
+        public bool Seleccionado
+        {
+            get { return seleccionado; }
+            set
+            {
+                // si esta bloqueado no se puede seleccionar
+                if (!bloqueado)
+                {
+                    seleccionado = value;
+                    BackgroundColor = seleccionado ? colorFondoSeleccionado : colorFondoNormal;
+                    TextColor = seleccionado ? colorTextoSeleccionado : colorTextoNormal;
+                    FontAttributes = seleccionado ? atributosSeleccionado : atributosNormal;
+                    Text = seleccionado ? letra : "-";
+                }
+            }
+        }
+
+        public bool Bloqueado
+        {
+            get { return bloqueado; }
+            set
+            {
+                bloqueado = value;
+                IsEnabled = bloqueado;
+                if (bloqueado)
+                {
+                    BackgroundColor = colorFondoBloqueado;
+                    TextColor = colorTextoBloqueado;
+                    FontAttributes = atributosBloqueado;
+                }
+                else
+                {
+                    // retornamos al color de si esta clickeado o no
+                    Seleccionado = Seleccionado;
+                }
+            }
+        }
+
+    }
+}
---
+++ Parejas Operativos/Parejas Operativos/MainPage.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Parejas_Operativos
+{
+    public partial class MainPage : ContentPage
+    {
+        // la lista de botones
+        List<List<BotonCasilla>> buttonList = new List<List<BotonCasilla>>();
+        List<List<string>> letras = new List<List<string>>();
+
+        List<Tuple<int, int>> parejas = new List<Tuple<int, int>>();
+        public MainPage()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        void generarLetras()
+        {
+            // en este ejemplo usaremos el abecedario
+            string caracteres = "abcdefghijklmnopqrstuvwxyz";
+            // hay que generar 30 letras(no duplicadas), duplicarlas y mezclarlas
+            List<string> lista = new List<string>();
+            // escogemos 30 letras al azar
+            for (int i = 0; i < 30; i++)
+            {
+                lista.Add(caracteres[new Random().Next(0, caracteres.Length)].ToString());
+            }
+            // duplicamos las letras
+            lista.AddRange(lista);
+            // mezclamos las letras
+            lista = lista.OrderBy(a => Guid.NewGuid()).ToList();
+            // guardamos las letras en el arreglo de letras
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    letras.Add(new List<string>());
+                    letras[i].Add(lista[i * 6 + j]);
+                }
+            }
+        }
+
+        void init()
+        {
+            generarLetras();
+
+            // creamos y agregamos los botones (6*6)
+            for (int i = 0; i < 6; i++)
+            {
+                List<BotonCasilla> filaBotones = new List<BotonCasilla>();
+                buttonList.Add(filaBotones);
+                for (int j = 0; j < 6; j++)
+                {
+                    BotonCasilla button = new BotonCasilla(i, j, letras[i][j]);
+                    filaBotones.Add(button);
+                    // agregamos el boton al grid
+                    gridBotones.Children.Add(button, j, i);
+                    // le asignamos el evento
+                    button.Clicked += (sender, e) =>
+                    {
+                        onBtnClick((BotonCasilla)sender);
+                    };
+                }
+            }
+        }
+
+        void onBtnClick(BotonCasilla boton)
+        {
+            int fila = boton.Fila;
+            int columna = boton.Columna;
+            // revisamos que el boton no este ya seleccionado
+            foreach (Tuple<int, int> casilla in parejas)
+            {
+                if (casilla.Item1 == fila && casilla.Item2 == columna)
+                {
+                    return;
+                }
+            }
+            // agregamos el boton a la lista de parejas
+            parejas.Add(new Tuple<int, int>(fila, columna));
+            // si ya hay 2 seleccionados hay que revisar si son pareja
+            if (parejas.Count > 2)
+            {
+                // revisamos si son pareja 
+                if (letras[parejas[0].Item1][parejas[0].Item2] == letras[parejas[1].Item1][parejas[1].Item2])
+                {
+                    // si son pareja los bloqueamos
+                    for (int i = 0; i < 2; i++)
+                    {
+                        buttonList[parejas[i].Item1][parejas[i].Item2].IsEnabled = false;
+                    }
+                    // los removemos de la lista de parejas
+                    parejas.RemoveAt(0);
+                    parejas.RemoveAt(0);
+                }
+                // si no son pareja
+                else
+                {
+                    // los dejamos de seleccionar
+                    for (int i = 0; i < 2; i++)
+                    {
+                        buttonList[parejas[i].Item1][parejas[i].Item2].Seleccionado = false;
+                    }
+                    // los removemos de la lista de parejas
+                    parejas.RemoveAt(0);
+                    parejas.RemoveAt(0);
+                }
+            }
+            // la ultima seleccionada siempre es de color verde limon #7bcb03
+            if (parejas.Count > 0)
+            {
+                // obtenemos la ultima seleccionada
+                int ultimaCasilla = parejas.Count - 1;
+                // la seleccionamos
+                buttonList[parejas[ultimaCasilla].Item1][parejas[ultimaCasilla].Item2].Seleccionado = true;
+
+            }
+        }
+    }
+}
